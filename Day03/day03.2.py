c7cc681e91128b0ad77cd2f9a8be5f99f0c1f356
James Orr <orr.james@gmail.com>
2019-12-03 19:31:11 -0500

    day 03 (2)

--- Day03/day03.2.py
+++ Day03/day03.2.py
@@ -8,19 +8,22 @@ def make_path(wire):
         'U': (0, 1),
         'D': (0, -1)
     }
-    visited = set()
+    visited = {}
     current = (0,0)
+    steps = 0
     moves = wire.split(',')
     for move in moves:
         direction = directions[move[0]]
         length = int(move[1:])
         for _ in range(0,length):
+            steps += 1
             current = (current[0] + direction[0], current[1] + direction[1])
-            visited.add(current)
+            if current not in visited:
+                visited[current] = steps
             
     return visited
 
 (path1, path2) = (make_path(wire1), make_path(wire2))
-intersections = path1.intersection(path2)
-distances = [abs(x[0]) + abs(x[1]) for x in intersections]
+intersections = set(path1.keys()).intersection(set(path2.keys()))
+distances = [path1[x] + path2[x] for x in intersections]
 print(min(distances))
