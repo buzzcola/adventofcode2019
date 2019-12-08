065ccdb5c7f8eaa0973003d39ff36edd8a3cfbb9
James Orr <orr.james@gmail.com>
2019-12-08 11:12:48 -0500

    Day 08: iterators are more efficient.

--- Day08/day08.2.py
+++ Day08/day08.2.py
@@ -10,8 +10,6 @@ layers = chunk(pixels, layer_size)
 columns = [[layer[i] for layer in layers] for i in range(layer_size)]
 
 for i in range(0, layer_size):
-    visible = [x for x in columns[i] if x != 2][0]
-    print(' ' if visible == 0 else 'X', end='')
-    
-    if (i+1) % width == 0:
-        print()
+    pixel = next(filter(lambda x: x != 2, columns[i]))
+    print(' ' if pixel == 0 else 'X', end='')    
+    if (i+1) % width == 0: print()
